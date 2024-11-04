6eb708b5ecc2bbe1568e7075e7cdd9d6276ccc7f
farman ansari <farmanansari35728@gmail.com>
2024-11-04 10:00:44 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,92 +1,93 @@
+// Que 1)   finding Grades.......................................
 
-// Que No. 1) - Add two Numbers........................................
-// function AddTwoNumbers(){
-//     let A = 5;
-//     let B = 2;
-//     let Sum = A + B;
-//     console.log(Sum);
-// }
-// AddTwoNumbers();
-
-
-//  Que No. 2) - Conditions Obeyed?.......................................
-// function Is_Valid(){
-//     let A = 5;
-//     let B = 3;
-//     if(A < 10 && A > B){
-//         console.log("true");
-//     }else{
-//         console.log("false");
+// const M = 1;
+// function findGrades(M) {
+//     switch (true) {
+//         case (M >= 0 && M <= 10):
+//             return 'E';
+//         case (M >= 11 && M <= 20):
+//             return 'D';
+//         case (M >= 21 && M <= 30):
+//             return 'C';
+//         case (M >= 31 && M <= 40):
+//             return 'B';
+//         case (M >= 41 && M <= 50):
+//             return 'A';
+//         default:
+//             return 'Invalid Marks'; 
 //     }
 // }
-// Is_Valid();
 
 
-//  Que No. 3) - Checking Conditions..........................................
-// function Check(){
-//     let A = 12;
-//     let B = 20;
-//     if(A % 10 == 0 || B % 10 == 0){
-//         console.log("true");
-//     }else{
-//         console.log("false");
+// console.log(findGrades(M));  
+
+
+
+// Que 2) Get value.....................................
+// const C = 'E';
+// function getValue(C) {
+
+//     const valueMap = {
+//         'P': 'PrepBytes',
+//         'p': 'PrepBytes',
+//         'Z': 'Zenith',
+//         'z': 'Zenith',
+//         'E': 'Expert Coder',
+//         'e': 'Expert Coder',
+//         'D': 'Data Structure',
+//         'd': 'Data Structure'
+//     };
+    
+//     return valueMap[C]
+// }
+
+// console.log(getValue(C));  
+ 
+
+
+// Que 3)  Maxcimum out of three number .............................................
+
+
+// const A = 2, B = 5, C = 4;
+// function Max_out_of_three(A, B, C) {
+   
+//     return Math.max(A, B, C);
+// }
+
+// console.log(Max_out_of_three(A, B, C));  
+
+
+
+
+
+// Que 4)  Second Smallest number............................................
+
+// const X = 2, Y = 9, Z = 23;
+// function findSmallest(X, Y, Z) {
+//     const numbers = [X, Y, Z];
+
+//     numbers.sort((a, b) => a - b);
+
+//     return numbers[1];
+// }
+
+// console.log(findSmallest(X, Y, Z));  
+
+
+
+// Que 5)  Check whether the tringle is acute or obtuse ...............................
+
+// const A = 60, B = 100, C = 20;
+// function Triangle_Check(A, B, C){
+     
+//     if (A + B + C !== 180) {
+//         return 'Invalid Triangle';
 //     }
+//     if (A > 70 || B > 70 || C > 70) {
+//         return 'obtuse';
+//     }
+
+//     return 'acute';
 // }
-// Check();
+// console.log(Triangle_Check(A, B, C)); 
 
-
-//  Que No. 4) - first digit of 4 digit numbers.................................
-// function First_Digit(){
-//     let N = "4567";
-//     console.log(N[0]);
-// }
-// First_Digit();
-
-
-//  Que No. 5) - last digit of 4 digit Number....................................
-// function Last_Digit(){
-//     let N = "4567";
-//     console.log(N[3]);
-// }
-// Last_Digit();
-
-
-//  Que No. 6) - Remainder........................................................
-// function Find_the_remainder(){
-//     let A = 2;
-//     let B = 9;
-//     let Rem = B % A;
-//     console.log(Rem);
-// }
-// Find_the_remainder();
-
-
-//  Que No. 7)  Multiply two Numbers..............................................
-// function Multiply_two_number(){
-//     let A = 2;
-//     let B = 5;
-//     let Mul = A * B;
-//     console.log(Mul);
-// }
-// Multiply_two_number();
-
-
-//  Que No. 8)  Marks Calculator.................................................
-// function Sum(){
-//     let Maths = 50;                // total marks..170
-//     let English = 20;
-//     let JavaScript = 100;
-//     let Sum = Maths + English + JavaScript;
-//     console.log(Sum);
-// }
-// Sum();
-
-// function Average(){           // this is a average..34
-//     let Maths = 50;
-//     let English = 20;
-//     let JavaScript = 100;
-//     let Sum = Maths + English + JavaScript;
-//     let Average = Sum/5;
-//     console.log(Average);
-// }
-// Average();
