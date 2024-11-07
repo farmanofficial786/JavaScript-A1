485d4f95cb66b1f68a46fdd2de785d61d06384f2
farman ansari <farmanansari35728@gmail.com>
2024-11-07 10:34:43 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,93 +1,53 @@
-// Que 1)   finding Grades.......................................
+//            DAILY  CLASS
 
-// const M = 1;
-// function findGrades(M) {
-//     switch (true) {
-//         case (M >= 0 && M <= 10):
-//             return 'E';
-//         case (M >= 11 && M <= 20):
-//             return 'D';
-//         case (M >= 21 && M <= 30):
-//             return 'C';
-//         case (M >= 31 && M <= 40):
-//             return 'B';
-//         case (M >= 41 && M <= 50):
-//             return 'A';
-//         default:
-//             return 'Invalid Marks'; 
+
+// Loops ....................................................................
+
+// (i). for loop
+// const n = 5;
+// for(let i = 1; i <= n; i++){
+//     console.log("I am a programmer");
+// }
+
+// Que - Write a program which takes a number from user and print the table. 
+
+// let i = 3;
+// for(let i = 1; i <= 10; i++){
+//     console.log(`3 x ${i} = ${3 * i}`);
+// }
+
+
+// Print a pattern ..............................................................
+
+// for(let i = 1; i <= 10; i++){
+//     let pattern = " ";
+//     for(let j = 1; j <= i; j++){
+//         pattern += j ; // or pattern += j + "";
 //     }
+//     console.log(pattern);
 // }
 
 
-// console.log(findGrades(M));  
+//  for-in loop ...........................................................
 
-
-
-// Que 2) Get value.....................................
-// const C = 'E';
-// function getValue(C) {
-
-//     const valueMap = {
-//         'P': 'PrepBytes',
-//         'p': 'PrepBytes',
-//         'Z': 'Zenith',
-//         'z': 'Zenith',
-//         'E': 'Expert Coder',
-//         'e': 'Expert Coder',
-//         'D': 'Data Structure',
-//         'd': 'Data Structure'
-//     };
-    
-//     return valueMap[C]
+// const student = {
+//     name : "Arhan",
+//     class : 4,
+//     age : 9
+// }
+// for(let key in student){
+// console.log(`${key} : ${student[key]}`);
 // }
 
-// console.log(getValue(C));  
- 
+
+// Nested loops ...................................................
 
 
-// Que 3)  Maxcimum out of three number .............................................
-
-
-// const A = 2, B = 5, C = 4;
-// function Max_out_of_three(A, B, C) {
-   
-//     return Math.max(A, B, C);
-// }
-
-// console.log(Max_out_of_three(A, B, C));  
-
-
-
-
-
-// Que 4)  Second Smallest number............................................
-
-// const X = 2, Y = 9, Z = 23;
-// function findSmallest(X, Y, Z) {
-//     const numbers = [X, Y, Z];
-
-//     numbers.sort((a, b) => a - b);
-
-//     return numbers[1];
-// }
-
-// console.log(findSmallest(X, Y, Z));  
-
-
-
-// Que 5)  Check whether the tringle is acute or obtuse ...............................
-
-// const A = 60, B = 100, C = 20;
-// function Triangle_Check(A, B, C){
-     
-//     if (A + B + C !== 180) {
-//         return 'Invalid Triangle';
+// const n = 8100;
+// for (let i = 1; i <= n; i++) {
+//     let str = '';
+//     for (let j = 1; j <= i; j++) {
+//         str += j + ' ';
 //     }
-//     if (A > 70 || B > 70 || C > 70) {
-//         return 'obtuse';
-//     }
-
-//     return 'acute';
+//     console.log(str);
 // }
-// console.log(Triangle_Check(A, B, C)); 
-
