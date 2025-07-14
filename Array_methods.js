1b5b6e2f6e72a19bdc8ab0b321ff20fa3bd34bc6
farman ansari <farmanansari35728@gmail.com>
2025-07-14 23:42:10 +0530

    Add files via upload

---
+++ Array_methods.js
@@ -0,0 +1,42 @@
+// JAVASCRIPT ASSINGMENT- 5
+//ARRAY METHODS...
+
+// Que 1) Find the Product..................................................................................
+
+// function Find_the_Product(arr){
+//    let product = 1;
+
+//     for(let num of arr){
+//       product *= num;
+//     }
+
+//     return product;
+
+// };
+
+// console.log(Find_the_Product([2, 3, 6,]));
+
+
+
+
+// Que 2) Find the sum......................................................................................
+
+// function Find_the_Sum(arr){
+
+//    let sum = 0;
+
+//    for(let num of arr){
+//       sum += num;
+//    }
+//    return sum; 
+// }
+
+// console.log(Find_the_Sum([2, 4, 6, 3, 5, 10]));
+
+
+
+
+// Que 3) Count Occurrences..................................................................................
+
+
+
